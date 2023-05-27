c198acaba2f7401fceed0b05b0278cbf7e181e55
Jisang Yun <journeyofbabo@gmail.com>
2023-05-28 06:02:38 +0900

    Refectoring with Structs

--- ex_struct1/src/main.rs
+++ ex_struct1/src/main.rs
@@ -1,4 +1,33 @@
-//Refactoring with Tuples
+//Refectoring with Structs: Adding more Meaning
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+fn main() {
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+    let rect2: Rectangle = Rectangle {
+        width: 35,
+        height: 55,
+    };
+
+    println!(
+        "The area of the rectangle1 is {} square pixels, 
+        rectangle2 is {}",
+        area(&rect1), area(&rect2)
+    );
+}
+
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
+}
+
+
+
+/*//Refactoring with Tuples
 fn main() {
     let rect1 = (30,50);
 
@@ -10,9 +39,7 @@ fn main() {
 
 fn area(dimensions: (u32, u32)) -> u32 {
     dimensions.0 * dimensions.1
-}
-
-
+}*/
 
 /*fn main() { //The basic way
     let width1 = 30;
