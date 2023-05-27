73da96af18571d9987528e62cfbff881c58700c7
Jisang Yun <journeyofbabo@gmail.com>
2023-05-27 23:00:00 +0900

    dele

--- exwith_Structs/target/debug/deps/exwithStructs-8075295b8f195452.d
+++
@@ -1,5 +0,0 @@
-/Users/jsyun/Programming/Rust/basicssRust/exwithStructs/target/debug/deps/exwithStructs-8075295b8f195452.rmeta: src/main.rs
-
-/Users/jsyun/Programming/Rust/basicssRust/exwithStructs/target/debug/deps/exwithStructs-8075295b8f195452.d: src/main.rs
-
-src/main.rs:
