2c608b0d03745c849c470420df276259df2fbee7
Jisang Yun <journeyofbabo@gmail.com>
2023-05-27 22:53:23 +0900

    add example Structs

---
+++ exwithStructs/target/debug/deps/exwithStructs-45cea841224379fe.d
@@ -0,0 +1,5 @@
+/Users/jsyun/Programming/Rust/basicssRust/exwithStructs/target/debug/deps/exwithStructs-45cea841224379fe.rmeta: src/main.rs
+
+/Users/jsyun/Programming/Rust/basicssRust/exwithStructs/target/debug/deps/exwithStructs-45cea841224379fe.d: src/main.rs
+
+src/main.rs:
