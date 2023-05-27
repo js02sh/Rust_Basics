d96eee60a929127de0c3e6d415868ae2b14ddf89
Jisang Yun <journeyofbabo@gmail.com>
2023-05-27 22:49:35 +0900

    delete

--- slice/slice0/target/debug/deps/slice0-4ebf606d621213af.d
+++
@@ -1,5 +0,0 @@
-/Users/jsyun/Programming/Rust/basicssRust/slice/slice0/target/debug/deps/slice0-4ebf606d621213af.rmeta: src/main.rs
-
-/Users/jsyun/Programming/Rust/basicssRust/slice/slice0/target/debug/deps/slice0-4ebf606d621213af.d: src/main.rs
-
-src/main.rs:
--- slice/slice0/target/debug/deps/slice0-eb1125a6816e99aa.d
+++
@@ -1,5 +0,0 @@
-/Users/jsyun/Programming/Rust/basicssRust/slice/slice0/target/debug/deps/slice0-eb1125a6816e99aa.rmeta: src/main.rs
-
-/Users/jsyun/Programming/Rust/basicssRust/slice/slice0/target/debug/deps/slice0-eb1125a6816e99aa.d: src/main.rs
-
-src/main.rs:
